7223d5f2d94bd4cf4177bc46a354d2db2bdbb818
mczkzk <k.mochizuki.0409@gmail.com>
2025-04-27 00:18:39 +0900

    fix

--- chapter3-2/main.tf
+++ chapter3-2/main.tf
@@ -131,3 +131,35 @@ resource "aws_security_group" "db" {
   }
 }
 
+# RDS
+resource "aws_db_instance" "wordpress" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "8.0.mysql_aurora.3.02.0"
+  instance_class       = "db.t3.micro"
+  db_name              = "wpdb"
+  username             = "dba"
+  password             = random_password.wordpress.result # Randomプロバイダーを使用
+  parameter_group_name = "default.mysql80"
+  multi_az             = false
+  db_subnet_group_name = aws_db_subnet_group.db.name
+  vpc_security_group_ids = [aws_security_group.db.id]
+  backup_retention_period = "7"
+  backup_window          = "01:00-02:00"
+  skip_final_snapshot  = true
+  max_allocated_storage = 200
+  identifier = "wordpress"
+  tags = {
+    Name = "Wordpress DB"
+  }
+}
+
+# DB Subnet Group
+resource "aws_db_subnet_group" "db" {
+  name       = "wordpress"
+  subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_c.id]
+  tags = {
+    Name = "Wordpress DB Subnet Group"
+  }
+}
