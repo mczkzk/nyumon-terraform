0fb268f932d8c33c543c2b79688d74d2ef185ae5
mczkzk <k.mochizuki.0409@gmail.com>
2025-04-26 15:50:19 +0900

    fix

---
+++ chapter3/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"  # Tokyo region
+}
+
+# Get the latest Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "test_server" {
+  ami           = data.aws_ami.amazon_linux_2023.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TestInstance"
+  }
+}
